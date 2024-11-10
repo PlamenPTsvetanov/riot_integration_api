6ba1d2090f5c6ccb77f2d78d276c1eb854184ba5
Plamen Tsvetanov <plamenptsvetanov@gmail.com>
2024-11-10 20:38:56 +0200

    Committing left over files

--- api/src/main/java/me/riot/integration/api/account/controllers/SummonerController.java
+++ api/src/main/java/me/riot/integration/api/account/controllers/SummonerController.java
@@ -2,10 +2,13 @@ package me.riot.integration.api.account.controllers;
 
 import me.riot.integration.api.account.dto.SummonerDTO;
 import me.riot.integration.api.account.services.SummonerService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
 
 @RestController
 @RequestMapping("/summoners")
@@ -19,9 +22,16 @@ public class SummonerController {
 
 
     @GetMapping(path = "")
-    public SummonerDTO getAccount(
+    public SummonerDTO getSummonerMetaData(
             @RequestParam("puuid") String puuid) {
         return service.getSummonerInfo(puuid);
+    }
+
+
+    @GetMapping(path = "/icon", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getIcon(
+            @RequestParam("iconId") BigInteger iconId) {
+        return service.getSummonerIcon(iconId);
     }
 }
 
